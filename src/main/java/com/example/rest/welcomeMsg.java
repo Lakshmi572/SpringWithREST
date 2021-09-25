ec0dd64f26d4c0b31c71ec16733185deeddf9ec7
Lakshmi <r131572@rguktrkv.ac.in>
2021-09-25 09:36:45 +0530

    first commit for rest

---
+++ src/main/java/com/example/rest/welcomeMsg.java
@@ -0,0 +1,16 @@
+package com.example.rest;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class welcomeMsg {
+
+    @GetMapping("/welcome")
+    public ResponseEntity<String> welcome(){
+        String Responsemsg="Welcome to the first app for Spring with Rest";
+        return new ResponseEntity<>(Responsemsg, HttpStatus.OK);
+    }
+}
